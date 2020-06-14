8bf9048ecaff3080cc0788b1875903941d09a42d
Ritwick <d.ritwick24@gmail.com>
2020-06-14 19:48:12 +0530

    first commit frontend

---
+++ php/admindata.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Home</title>
+<link href="style/index-layout.css" rel="stylesheet" type="text/css" />
+<link href="style/homepage-layout.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+
+<!--right-->
+
+<br><br>
+<P ><B><h2 align="center">Admin Pannel</h2></FONT></B></P>
+
+</div></div>
+<table border="2" align="center">
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>phone</th>
+          <th>date</th>
+          <th>email</th>
+          <th>garden</th>
+          <th>ACCEPT</th>
+          <th>REJECT</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+                    <tr>
+              <td>Mohit</td>
+              <td>1234567890</td>
+              <td>2019-04-06</td>
+              <td>2017pietcsritwick088</td>
+              <td></td>
+              <td><button>accept</button></td>
+              <td><a href="reject.php?id=2017pietcsritwick088&date="><button>reject</button></a></td>
+            </tr>
+                      <tr>
+              <td>Mohit</td>
+              <td>1234567890</td>
+              <td>2019-04-06</td>
+              <td>2017pietcsritwick088</td>
+              <td></td>
+              <td><button>accept</button></td>
+              <td><a href="reject.php?id=2017pietcsritwick088&date="><button>reject</button></a></td>
+            </tr>
+                      <tr>
+              <td>Mohit</td>
+              <td>1234567890</td>
+              <td>2019-04-03</td>
+              <td>2017pietcsritwick088</td>
+              <td>jamuna reseort</td>
+              <td><button>accept</button></td>
+              <td><a href="reject.php?id=2017pietcsritwick088&date="><button>reject</button></a></td>
+            </tr>
+                      <tr>
+              <td>        </td>
+              <td>0000000000</td>
+              <td>2020-02-29</td>
+              <td>sana@er.w</td>
+              <td>jamuna reseort</td>
+              <td><button>accept</button></td>
+              <td><a href="reject.php?id=sana@er.w&date="><button>reject</button></a></td>
+            </tr>
+                      <tr>
+              <td>qw</td>
+              <td>909898</td>
+              <td>1999-02-12</td>
+              <td>q@qw</td>
+              <td>gangotri gardena</td>
+              <td><button>accept</button></td>
+              <td><a href="reject.php?id=q@qw&date="><button>reject</button></a></td>
+            </tr>
+                </tbody>
+    </table>
+     <br/><br/>
+
+</body>
+</html>
